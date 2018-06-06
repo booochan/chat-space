1fc57183add2e4e7096eaa937aaebc7184e89252
booochan <omocha@outlook.com>
2018-06-06 18:21:15 +0900

    Add users_controller & add action edit & update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :email,
+      )
+  end
 end
