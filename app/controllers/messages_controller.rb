9841d0a7a9d6d71deb3e7cf9adf2a98d3508b463
booochan <omocha@outlook.com>
2018-06-10 17:15:19 +0900

    Edit messages_controller create action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,5 +5,15 @@ class MessagesController < ApplicationController
   end
 
   def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(@message.group_id)
+    else
+      render :index
+  end
+
+  private
+  def massage_params
+    params.require(:message).permit(:text, :image).merge(user_id: current_user.id, group_id: params[:group_id])
   end
 end
