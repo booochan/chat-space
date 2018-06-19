7e5a1ec3b02e97a2950b4445a5ba32a8205fd4e2
booochan <omocha@outlook.com>
2018-06-19 17:34:16 +0900

    Add search action in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update]
+  before_action :authenticate_user!, only: [:search]
 
   def edit
   end
@@ -9,6 +10,14 @@ class UsersController < ApplicationController
       redirect_to root_path, notice: 'ユーザー情報を更新しました'
     elsif
       render :edit
+    end
+  end
+
+  def search
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    respond_to do |format|
+      format.html
+      format.json
     end
   end
 
